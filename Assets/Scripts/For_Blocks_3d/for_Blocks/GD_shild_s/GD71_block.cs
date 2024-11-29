2db15c5ff2ed3a6978cadf8e893f05d06d1d22ff
__ANTON_2002__ <101361041+ANTON2002ARS@users.noreply.github.com>
2024-11-29 15:48:56 +0400

    add shild GD

---
+++ Assets/Scripts/For_Blocks_3d/for_Blocks/GD_shild_s/GD71_block.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GD71_block : Abst_Block
+{
+    public override List<switch_position> Action_Toggles { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public override List<switch_position> Need_Condition { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public override void Check_Result()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Del_status(switch_position switch_position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Set_Status(switch_position switch_position)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/For_Blocks_3d/for_Blocks/GD_shild_s/vch73_Block.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vch73_Block : Abst_Block
+{
+    [SerializeField] private List<switch_position> pos;
+    public override List<switch_position> Action_Toggles { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public override List<switch_position> Need_Condition { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public override void Check_Result()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Del_status(switch_position switch_position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Set_Status(switch_position switch_position)
+    {
+        throw new System.NotImplementedException();
+    }
+}
